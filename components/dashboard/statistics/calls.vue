7109f9a4fcd9ff1004d916ffc837d945c63c87a8
Лесхан <l.karataev@oyster-telecom.ru>
2020-02-26 15:07:00 +0300

    fixes and apexchart & sms chart

--- components/dashboard/statistics/calls.vue
+++ components/dashboard/statistics/calls.vue
@@ -1,101 +1,43 @@
 <template>
-  <div class="row no-gutters">
-    <div class="col-lg-12">
-      <div ref="calls_line_chart"></div>
+  <div class="row">
+    <div class="col">
+      <apexchart type="area" height="240" :options="chartOptions" :series="series"/>
     </div>
-    <!-- <div class="col-lg-3">
-      <div ref="calls_pie_chart"></div>
-    </div> -->
   </div>
 </template>
 
 <script>
-import ApexCharts from "apexcharts";
 export default {
-  mounted() {
-    this.getRandomData();
-
-    const calls_line_chart = new ApexCharts(
-      this.$refs["calls_line_chart"],
-      this.calls_line_chart_data
-    );
-    const calls_pie_chart = new ApexCharts(
-      this.$refs["calls_pie_chart"],
-      this.calls_pie_chart_data
-    );
-
-    calls_line_chart.render();
-    calls_pie_chart.render();
-  },
-  data: () => ({
-    calls_data: [],
-    auth_data: []
-  }),
-  computed: {
-    calls_pie_chart_data: function() {
-      return {
-        chart: {
-          type: "pie",
-          height: "300px"
+  data() {
+    return {
+      series: [
+        {
+          name: "Запросов",
+          data: [11, 32, 45, 32, 34, 52, 41]
         },
-        legend: {
-          position: "bottom"
+        {
+          name: "Авторизаций",
+          data: [31, 40, 28, 51, 42, 109, 100]
         },
-        series: [300, 50],
-        labels: ["Запросов", "Авторизаций"],
-        dataLabels: {
-          enabled: false
-        }
-      };
-    },
-    calls_line_chart_data: function() {
-      return {
+      ],
+      chartOptions: {
         chart: {
-          type: "area",
-          height: "240",
-          width: "100%"
+          height: 240,
+          width:600,
+          type: 'area'
         },
         dataLabels: {
           enabled: false
         },
         stroke: {
-          curve: "smooth"
+          curve: 'smooth'
         },
-        series: [
-          {
-            name: "Запросов",
-            data: this.calls_data
-          },
-          {
-            name: "Авторизаций",
-            data: this.auth_data
-          }
-        ],
-        xaxis: this.calls_xaxis_categories
-      };
-    },
-    calls_xaxis_categories: function() {
-      let array = [];
-      for (let i = 1; i <= 20; i++) {
-        array.push(String(i));
-      }
-      return { categories: array };
-    },
-  },
-  methods: {
-    getRandomData () {
-      this.auth_data = this.randomData();
-      this.calls_data = this.randomData();
-    },
-    randomData () {
-      const min = 0;
-      const max = 300;
-      let data = [];
-      for (let i = 1; i <= 20; i++) {
-        data.push(Math.floor(Math.random() * (max - min)) + min);
-      }
-      return data;
+        xaxis: {
+          categories: [1,2,3,4,5,6,7]
+        },
+      },
+
     }
-  }
+  },
 };
 </script>
