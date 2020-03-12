ae58c26e019e82d473823de43596d7d4320e0dfa
Лесхан <l.karataev@oyster-telecom.ru>
2020-03-12 11:42:14 +0300

    :wrench: Changing configuration files.

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,14 +10,13 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || 'Oyspot',
+    title: process.env.APP_NAME || 'OySpot',
     meta: [
       {charset: 'utf-8'},
       {name: 'viewport', content: 'width=device-width, initial-scale=1'},
       {hid: 'description', name: 'description', content: process.env.npm_package_description || ''}
     ],
     link: [
-      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
       {rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png',},
       {rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png',},
       {rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicon-96x96.png',},
