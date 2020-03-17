f1fd3b25607025d0f6b038d64263467fb2ffc40d
leskhantos <leskhan001@gmail.com>
2020-03-17 23:15:46 +0300

    :construction_worker: Adding interface for client.

---
+++ pages/dashboard/spot.vue
@@ -0,0 +1,16 @@
+<template>
+    <oy-page>
+      <oy-page-header title="Зоны"/>
+    </oy-page>
+</template>
+
+<script>
+    export default {
+        name: "spot",
+      layout:'dashboard'
+    }
+</script>
+
+<style scoped>
+
+</style>
